30996631e8099ffa3fc9c63831383f2bb616475f
ahmed <ahmedhalwgy@gmail.com>
2024-02-04 18:08:15 +0200

    Views Refactor

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
-import { UserForm } from './views/UserForm';
-import { User } from './models/User';
+import { UserList } from './views/UserList';
+import { Collection } from './models/Collection';
+import { UserProbs, User } from './models/User';
 
-const user = User.buildUser({ name: 'Ahmed', age: 29 });
+const users = new Collection(
+  'http://localhost:3000/users',
+  (json: UserProbs) => {
+    return User.buildUser(json);
+  }
+);
 
-const rootElement = document.getElementById('root');
-if (rootElement) {
-  const userForm = new UserForm(rootElement, user);
-  userForm.render();
-} else {
-  console.error("Element with ID 'root' not found.");
-}
+users.on('change', () => {
+  const root = document.getElementById('root');
+
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
+
+users.fetch();
